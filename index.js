f1231063014738e6c165faf1cc9782a90fd508b4
A-for-Anders <46354715+A-for-Anders@users.noreply.github.com>
2019-01-28 00:37:40 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 var button = document.getElementById("button-key-1");
 
 button.addEventListener("click", function() {
-    document.location.href = "https://a-for-anders.github.io/WeeklyUpdate_GithubTestLab/projects/clock/clock.html";
+      document.location.href = "https://a-for-anders.github.io/WeeklyUpdate_GithubTestLab/PROJECTS/CLOCK/clock.html";
 });
